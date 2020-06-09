c539e6b05f25ebb706670b5a783a6fb0fa76af48
Theo Lipeles <theo@macbook-pro.myfiosgateway.com>
2020-06-08 20:39:15 -0400

    add jsdoc comments for helper types

--- src/classes/helpers.ts
+++ src/classes/helpers.ts
@@ -8,7 +8,17 @@ export function clamp(num:number, a:number, b:number):number {
     return Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b))
 };
 
-export type OrganismFunction = (populations: Populations) => number;
+/**
+ * Maps organism sequence to population size
+ */
 export type Populations = Map<string, number>;
+/**
+ * maps abiotic factor name to quantity
+ */
 export type Factors = Map<string, number>;
+/**
+ * effect of factor or trait on another trait given a factor or organism quantity
+ */
 export type Effect = (quantity: number) => number;
+
+export type OrganismFunction = (populations: Populations) => number;
