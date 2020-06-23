8532a5e6026500d30fc36d5fbd728f12991fac3e
Theo Lipeles <theo@macbook-pro.lan>
2020-06-23 17:28:56 -0400

    add mutation chance to Trait

--- src/classes/Trait.class.ts
+++ src/classes/Trait.class.ts
@@ -9,14 +9,19 @@ import { Effect } from "./helpers";
  */
 export class Trait {
     name: string;
-    sequence:string;
+    sequence: string;
+    mutationChance: number;
     traitEffects: Map<Trait, Effect>;
     factorEffects: Map<string, Effect>;
     children: Set<Trait>;
 
-    constructor(name:string, sequence:string) {
+    constructor(name:string, sequence:string, mutationChance=0) {
         this.name = name;
         this.sequence = sequence;
+        if (mutationChance < 0 || mutationChance > 1) {
+            throw new Error(`mutationChance must be between 0 and 1. Instead got: ${mutationChance}`);
+        }
+        this.mutationChance = mutationChance;
         this.traitEffects = new Map<Trait, Effect>();
         this.factorEffects = new Map<string, Effect>();
         this.children = new Set<Trait>();
